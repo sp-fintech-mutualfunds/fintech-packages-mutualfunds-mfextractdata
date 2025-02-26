5eb8dce5b912ea6f82284864c23e87aaede16381
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-26 15:35:31 +1100

    update

--- MfExtractdata.php
+++ MfExtractdata.php
@@ -3,13 +3,18 @@
 namespace Apps\Fintech\Packages\Mf\Extractdata;
 
 use Apps\Fintech\Packages\Mf\Extractdata\Settings;
+use Apps\Fintech\Packages\Mf\Navs\MfNavs;
 use League\Flysystem\FilesystemException;
 use League\Flysystem\UnableToCheckExistence;
 use League\Flysystem\UnableToWriteFile;
+use Phalcon\Db\Enum;
 use System\Base\BasePackage;
+use System\Base\Providers\DatabaseServiceProvider\Sqlite;
 
 class MfExtractdata extends BasePackage
 {
+    protected $now;
+
     protected $sourceDir = 'apps/Fintech/Packages/Mf/Extractdata/Data/';
 
     protected $sourceLink = 'https://github.com/captn3m0/historical-mf-data/releases/latest/download/funds.db.zst';
@@ -24,6 +29,8 @@ class MfExtractdata extends BasePackage
 
     protected $settings = Settings::class;
 
+    protected $navsPackage;
+
     public function onConstruct()
     {
         if (!is_dir(base_path($this->sourceDir))) {
@@ -32,18 +39,17 @@ class MfExtractdata extends BasePackage
             }
         }
 
-        // /etc/apache2.conf - Change the timeout to 3600 else you will get Gateway Timeout, revert back when done to 300 (5 mins)
-        // Timeout 3600
-
-        //Increase Exectimeout to 20 mins as this process takes time to extract and merge data.
-        if ((int) ini_get('max_execution_time') < 3600) {
-            set_time_limit(3600);
+        //Increase Exectimeout to 5 hours as this process takes time to extract and merge data.
+        if ((int) ini_get('max_execution_time') < 18000) {
+            set_time_limit(18000);
         }
 
-        //Increase memory_limit to 2G as the process takes a bit of memory to process the array.
-        if ((int) ini_get('memory_limit') < 2048) {
-            ini_set('memory_limit', '2048M');
+        //Increase memory_limit to 1G as the process takes a bit of memory to process the array.
+        if ((int) ini_get('memory_limit') < 1024) {
+            ini_set('memory_limit', '1024M');
         }
+
+        $this->now = \Carbon\Carbon::now();
 
         parent::onConstruct();
     }
@@ -67,31 +73,29 @@ class MfExtractdata extends BasePackage
         }
     }
 
-    protected function downloadMfData($reDownload = false)
+    protected function downloadMfData()
     {
-        if (!$reDownload) {
-            try {
-                $file = $this->localContent->fileExists($this->sourceDir . 'funds.db.zst');
+        $today = $this->now->toDateString();
 
-                if ($file) {
-                    $fileModificationTime = $this->localContent->lastModified($this->sourceDir . 'funds.db.zst');
-
-                    if (\Carbon\Carbon::now()->timestamp < \Carbon\Carbon::parse($fileModificationTime)->addDay()->timestamp) {
-                        $this->addResponse('File was downloaded less than a day ago. Please use redownload to force redownload.', 1);
-
-                        return false;
-                    }
-                }
-            } catch (FilesystemException | UnableToCheckExistence | UnableToRetrieveMetadata | \throwable $e) {
-                $this->addResponse($e->getMessage(), 1);
-
-                return false;
+        try {
+            //File is already extracted
+            if ($this->localContent->fileExists($this->sourceDir . $today . '-funds.db')) {
+                return true;
             }
+
+            //File is already downloaded
+            if ($this->localContent->fileExists($this->sourceDir . $today . '-funds.db.zst')) {
+                return true;
+            }
+        } catch (FilesystemException | UnableToCheckExistence | UnableToRetrieveMetadata | \throwable $e) {
+            $this->addResponse($e->getMessage(), 1);
+
+            return false;
         }
 
         $this->method = 'downloadMfData';
 
-        return $this->downloadData($this->sourceLink, base_path($this->sourceDir) . 'funds.db.zst');
+        return $this->downloadData($this->sourceLink, base_path($this->sourceDir) . $today . '-funds.db.zst');
     }
 
     protected function downloadData($url, $sink)
@@ -151,15 +155,33 @@ class MfExtractdata extends BasePackage
 
     protected function extractMfData()
     {
+        $this->method = 'extractMfData';
+
+        $today = $this->now->toDateString();
+
         try {
-            $file = $this->localContent->fileExists($this->sourceDir . 'funds.db.zst');
+            //File is already extracted
+            if ($this->localContent->fileExists($this->sourceDir . $today . '-funds.db')) {
+                return true;
+            }
+
+            $file = $this->localContent->fileExists($this->sourceDir . $today . '-funds.db.zst');
 
             if (!$file) {
                 $this->addResponse('File not downloaded correctly', 1);
 
                 return false;
             }
-            exec('unzstd -d -f ' . base_path($this->sourceDir) . 'funds.db.zst -o ' . base_path($this->sourceDir) . 'funds.db', $output, $result);
+            exec('unzstd -d -f ' . base_path($this->sourceDir) . $today . '-funds.db.zst -o ' . base_path($this->sourceDir) . $today . '-funds.db', $output, $result);
+            $this->basepackages->progress->updateProgress($this->method, null, false, null, ['stepsTotal' => 5, 'stepsCurrent' => 1]);
+            exec("echo 'CREATE INDEX \"nav-main\" ON \"nav\" (\"date\",\"scheme_code\")' | sqlite3 " . base_path($this->sourceDir) . $today . "-funds.db", $output, $result);
+            $this->basepackages->progress->updateProgress($this->method, null, false, null, ['stepsTotal' => 5, 'stepsCurrent' => 2]);
+            exec("echo 'CREATE INDEX \"nav-scheme\" ON \"nav\" (\"scheme_code\")' | sqlite3 " . base_path($this->sourceDir) . $today . "-funds.db", $output, $result);
+            $this->basepackages->progress->updateProgress($this->method, null, false, null, ['stepsTotal' => 5, 'stepsCurrent' => 3]);
+            exec("echo 'CREATE INDEX \"securities-scheme\" ON \"securities\" (\"scheme_code\")' | sqlite3 " . base_path($this->sourceDir) . $today . "-funds.db", $output, $result);
+            $this->basepackages->progress->updateProgress($this->method, null, false, null, ['stepsTotal' => 5, 'stepsCurrent' => 4]);
+            exec("echo 'CREATE INDEX \"securities-isin\" ON \"securities\" (\"isin\")' | sqlite3 " . base_path($this->sourceDir) . $today . "-funds.db", $output, $result);
+            $this->basepackages->progress->updateProgress($this->method, null, false, null, ['stepsTotal' => 5, 'stepsCurrent' => 5]);
 
             if ($result !== 0) {
                 $this->addResponse('Error extracting file', 1, ['output' => $output]);
@@ -171,6 +193,158 @@ class MfExtractdata extends BasePackage
 
             return false;
         }
+
+        return true;
+    }
+
+    protected function processMfData()
+    {
+        $this->method = 'processMfData';
+
+        $today = $this->now->toDateString();
+
+        try {
+            //File is already extracted
+            if (!$this->localContent->fileExists($this->sourceDir . $today . '-funds.db')) {
+                $this->addResponse('File not downloaded and extracted correctly!', 1);
+
+                return false;
+            }
+        } catch (FilesystemException | UnableToCheckExistence | \throwable $e) {
+            $this->addResponse($e->getMessage(), 1);
+
+            return false;
+        }
+
+        try {
+            $sqlite = (new Sqlite())->init(base_path($this->sourceDir . $today . '-funds.db'));
+        } catch (\throwable $e) {
+            $this->addResponse('Unable to open database file', 1);
+
+            return false;
+        }
+
+        //Test
+        try {
+        $this->basepackages->utils->setMicroTimer('DBStart', true);
+
+        $isinNavs =
+            $sqlite->query(
+                "SELECT * from nav N
+                JOIN securities S ON N.scheme_code = S.scheme_code
+                WHERE S.isin = 'INF760K01FV4'
+                AND N.date >= '2000-01-01'
+                ORDER BY N.date DESC"
+            )->fetchAll(Enum::FETCH_ASSOC);
+
+        $this->basepackages->utils->setMicroTimer('DBStop', true);
+        trace(varsToDump : [$this->basepackages->utils->getMicroTimer()], exit: false, dumpTraces: false);
+
+        $this->basepackages->utils->resetMicroTimer();
+
+        $this->basepackages->utils->setMicroTimer('FFReadStart', true);
+        $this->navsPackage = new MfNavs;
+        $new = $this->navsPackage->getMfNavsByIsin('INF760K01FV4');
+        $this->basepackages->utils->setMicroTimer('FFReadStop', true);
+        trace(varsToDump : [$this->basepackages->utils->getMicroTimer()], exit: false, dumpTraces: false);
+
+        $this->basepackages->utils->resetMicroTimer();
+
+        $this->basepackages->utils->setMicroTimer('FFReadStart2', true);
+        $this->navsPackage = new MfNavs;
+        $new = $this->navsPackage->getById(1);
+        $this->basepackages->utils->setMicroTimer('FFReadStop2', true);
+        trace(varsToDump : [$this->basepackages->utils->getMicroTimer()], exit: false, dumpTraces: false);
+    } catch (\throwable $e) {
+        trace([$e]);
+    }
+        die();
+        //Test
+        try {
+            $this->navsPackage = new MfNavs;
+
+            $isins = $sqlite->query("SELECT * from securities")->fetchAll(Enum::FETCH_ASSOC);
+
+            if ($isins && count($isins) > 0) {
+                $isinsTotal = 1000;
+                foreach ($isins as $key => $isin) {
+                    $this->basepackages->utils->setMicroTimer('Start');
+                    $dbIsin = $this->navsPackage->getMfNavsByIsin($isin['isin']);
+
+                    // $lastUpdated = $this->now->subDay(1)->toDateString();
+                    $lastUpdated = '2000-01-01';
+
+                    if (!$dbIsin) {
+                        $dbIsin = [];
+                        $dbIsin['type'] = $isin['type'];
+                        $dbIsin['scheme_code'] = $isin['scheme_code'];
+                        $dbIsin['isin'] = $isin['isin'];
+                        $dbIsin['navs'] = [];
+                    } else {
+                        $lastUpdated = $dbIsin['last_updated'];
+                    }
+
+                    $isin = $isin['isin'];
+                    // $this->basepackages->utils->setMicroTimer('DBStart', true);
+                    $isinNavs =
+                        $sqlite->query(
+                            "SELECT * from nav N
+                            JOIN securities S ON N.scheme_code = S.scheme_code
+                            WHERE S.isin = '$isin'
+                            AND N.date >= '$lastUpdated'
+                            ORDER BY N.date ASC"
+                        )->fetchAll(Enum::FETCH_ASSOC);
+
+                    // $this->basepackages->utils->setMicroTimer('DBStop', true);
+                    // trace(varsToDump : [$this->basepackages->utils->getMicroTimer()], exit: false, dumpTraces: false);
+                    if ($isinNavs && count($isinNavs) > 0) {
+                        $dbIsin['last_updated'] = $this->helper->last($isinNavs)['date'];
+                        $dbIsin['latest_nav'] = $this->helper->last($isinNavs)['nav'];
+                        foreach ($isinNavs as $isinNav) {
+                            $dbIsin['navs'][$isinNav['date']] = $isinNav['nav'];
+                        }
+                    }
+
+                    if (isset($dbIsin['id'])) {
+                        $this->navsPackage->update($dbIsin);
+                    } else {
+                        $this->navsPackage->addMfNavs($dbIsin);
+                    }
+                    // $this->basepackages->utils->setMicroTimer('FFStop', true);
+                    // trace(varsToDump : [$this->basepackages->utils->getMicroTimer()], exit: false, dumpTraces: false);
+
+                    // $this->basepackages->utils->setMicroTimer('FFReadStart', true);
+                    // $new = $this->navsPackage->getMfNavsByIsin($isin);
+                    $this->basepackages->utils->setMicroTimer('End');
+
+                    $time = $this->basepackages->utils->getMicroTimer();
+
+                    if ($time && isset($time[1]['difference']) && $time[1]['difference'] !== 0) {
+                        $totalTime = date("H:i:s", floor($time[1]['difference'] * ($isinsTotal - $key)));
+                        // $this->basepackages->utils->formatMicrotime($time[1]['difference'] * $isinsTotal);
+                    }
+
+                    $this->basepackages->utils->resetMicroTimer();
+                    // trace(varsToDump : [$this->basepackages->utils->getMicroTimer()], exit: true, dumpTraces: false);
+                    $this->basepackages->progress->updateProgress(
+                        method: $this->method,
+                        counters: ['stepsTotal' => $isinsTotal, 'stepsCurrent' => ($key + 1)],
+                        text: 'Time remaining : ' . $totalTime . '...'
+                    );
+                    if ($key === 1000) {
+                        return true;
+                    }
+                }
+            }
+        } catch (\throwable $e) {
+            trace([$e]);
+        }
+
+        return true;
+        // $statement = $sqlite->prepare('SELECT * from nav LIMIT 0,1');
+        // trace([$sqlite->query('SELECT * from securities LIMIT 0,10')->fetchAll(Enum::FETCH_ASSOC)]);
+        // trace([$sqlite->query("SELECT * from nav N JOIN securities S ON N.scheme_code = S.scheme_code WHERE S.isin = 'INF209KB1ZL4' LIMIT 0,10")->fetchAll(Enum::FETCH_ASSOC)]);
+        // trace([$sqlite->query("SELECT * from nav N JOIN securities S ON N.scheme_code = S.scheme_code WHERE S.isin = 'INF209KB1ZL4'")->fetchAll(Enum::FETCH_ASSOC)]);
     }
 
     public function sync($data)
@@ -227,8 +401,19 @@ class MfExtractdata extends BasePackage
 
     protected function processGold($data)
     {
-        $now = \Carbon\Carbon::now();
-        $today = $now->toDateString();
+        $today = $this->now->toDateString();
+
+        try {
+            if ($this->localContent->fileExists($this->sourceDir . 'gold-' . $today . '.json')) {
+                $this->addResponse('File for ' . $today . ' already exists and imported.', 1);
+
+                return false;
+            }
+        } catch (\throwable | UnableToCheckExistence $e) {
+            $this->addResponse($e->getMessage(), 1);
+
+            return false;
+        }
 
         $days = 10;
         if (isset($data['days'])) {
